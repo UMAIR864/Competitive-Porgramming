64d6e994ed61a1e3dd16b94341071e40ce813314
UMAIR HOSSAIN <107942454+UMAIR864@users.noreply.github.com>
2022-12-16 10:33:59 +0600

    Umair Hossain

---
+++ BeeCrowd Solution/1059 Even Numbers.cpp
@@ -0,0 +1,14 @@
+//Solution by Umair Hossain
+#include<iostream>
+using namespace std;
+int main()
+{
+for(int i=0; i<=100; i=i+2)
+{
+    if(i==0)
+    {
+        continue;
+    }
+    cout<<i<<"\n";
+}
+}

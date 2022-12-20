6084a7774880fcc574de4be9c34fd71e6ca78c6e
UMAIR HOSSAIN <107942454+UMAIR864@users.noreply.github.com>
2022-12-20 12:17:09 +0600

    Umair Hossain

---
+++ Codeforces Solution/A. A+B_Contest_1772.cpp
@@ -0,0 +1,15 @@
+//Bismillahir Rahmanir Rahim.
+//Solution by Umair Hossain.
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;cin>>t;
+    while(t--)
+    {
+        cin.ignore();
+        string s;
+        cin>>s;
+        cout<<(s[0]+s[2])-48*2<<endl;
+    }
+}

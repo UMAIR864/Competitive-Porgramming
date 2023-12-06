d3731d32e7a0d27510c4120574063418244c3697
UMAIR HOSSAIN <107942454+UMAIR864@users.noreply.github.com>
2023-12-06 21:56:31 +0600

    Prime 1

--- Number Theory/01. Prime Number/PrimeNormal1.cpp
+++ Number Theory/01. Prime Number/PrimeNormal1.cpp
@@ -1,6 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
+int isPrime(int n)
+{
+    if(n<=1) return 0;
+    for(int i=2; i<n; i++)
+    {
+        if(n%i==0)
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
 int main()
 {
-
+    int n;
+    cout<<"Enter number: ";
+    cin>>n;
+    if(isPrime(n)==1)
+    {
+        cout<<n<<" is Prime.\n";
+    }
+    else
+    {
+        cout<<n<<" is Not Prime.\n";
+    }
+    return 0;
 }
